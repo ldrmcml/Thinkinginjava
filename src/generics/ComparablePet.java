8f2be6d2c74a8572ebdcc07bb6ad5d5c295d5ed2
ldrmcml <ldrmcml@qq.com>
2014-11-29 22:09:40 +0800

    merge

---
+++ src/generics/ComparablePet.java
@@ -0,0 +1,8 @@
+package generics;
+
+//: generics/ComparablePet.java
+
+public class ComparablePet
+implements Comparable<ComparablePet> {
+  public int compareTo(ComparablePet arg) { return 0; }
+} ///:~
