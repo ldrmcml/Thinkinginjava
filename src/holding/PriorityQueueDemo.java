4deab5bfd2ecdeeb56f208e08775fdcf31188bf8
陈明龙 <ldrmcml@qq.com>
2015-05-31 15:44:40 +0800

    20150531

--- src/holding/PriorityQueueDemo.java
+++ src/holding/PriorityQueueDemo.java
@@ -1,41 +1,45 @@
+package holding;
+
 //: holding/PriorityQueueDemo.java
 import java.util.*;
 
 public class PriorityQueueDemo {
-  public static void main(String[] args) {
-    PriorityQueue<Integer> priorityQueue =
-      new PriorityQueue<Integer>();
-    Random rand = new Random(47);
-    for(int i = 0; i < 10; i++)
-      priorityQueue.offer(rand.nextInt(i + 10));
-    QueueDemo.printQ(priorityQueue);
+	public static void main(String[] args) {
+		PriorityQueue<Integer> priorityQueue =
+				new PriorityQueue<Integer>();
+		Random rand = new Random(47);
+		for(int i = 0; i < 10; i++)
+			priorityQueue.offer(rand.nextInt(i + 10));
+		QueueDemo.printQ(priorityQueue);
 
-    List<Integer> ints = Arrays.asList(25, 22, 20,
-      18, 14, 9, 3, 1, 1, 2, 3, 9, 14, 18, 21, 23, 25);
-    priorityQueue = new PriorityQueue<Integer>(ints);
-    QueueDemo.printQ(priorityQueue);
-    priorityQueue = new PriorityQueue<Integer>(
-        ints.size(), Collections.reverseOrder());
-    priorityQueue.addAll(ints);
-    QueueDemo.printQ(priorityQueue);
+		List<Integer> ints = Arrays.asList(25, 22, 20,
+				18, 14, 9, 3, 1, 1, 2, 3, 9, 14, 18, 21, 23, 25);
+		priorityQueue = new PriorityQueue<Integer>(ints);
+		QueueDemo.printQ(priorityQueue);
+		priorityQueue = new PriorityQueue<Integer>(
+				ints.size(), Collections.reverseOrder());
+		// 上面声明优先队列，后面再加入数字
+		priorityQueue.addAll(ints);
+		QueueDemo.printQ(priorityQueue);
 
-    String fact = "EDUCATION SHOULD ESCHEW OBFUSCATION";
-    List<String> strings = Arrays.asList(fact.split(""));
-    PriorityQueue<String> stringPQ =
-      new PriorityQueue<String>(strings);
-    QueueDemo.printQ(stringPQ);
-    stringPQ = new PriorityQueue<String>(
-      strings.size(), Collections.reverseOrder());
-    stringPQ.addAll(strings);
-    QueueDemo.printQ(stringPQ);
+		String fact = "EDUCATION SHOULD ESCHEW OBFUSCATION";
+		// 3个空格也被分为三个元素
+		List<String> strings = Arrays.asList(fact.split(""));
+		PriorityQueue<String> stringPQ =
+				new PriorityQueue<String>(strings);
+		QueueDemo.printQ(stringPQ);
+		stringPQ = new PriorityQueue<String>(
+				strings.size(), Collections.reverseOrder());
+		stringPQ.addAll(strings);
+		QueueDemo.printQ(stringPQ);
 
-    Set<Character> charSet = new HashSet<Character>();
-    for(char c : fact.toCharArray())
-      charSet.add(c); // Autoboxing
-    PriorityQueue<Character> characterPQ =
-      new PriorityQueue<Character>(charSet);
-    QueueDemo.printQ(characterPQ);
-  }
+		Set<Character> charSet = new HashSet<Character>();
+		for(char c : fact.toCharArray())
+			charSet.add(c); // Autoboxing
+		PriorityQueue<Character> characterPQ =
+				new PriorityQueue<Character>(charSet);
+		QueueDemo.printQ(characterPQ);
+	}
 } /* Output:
 0 1 1 1 1 1 3 5 8 14
 1 1 2 3 3 9 9 14 14 18 18 20 21 22 23 25 25
@@ -43,4 +47,4 @@ public class PriorityQueueDemo {
        A A B C C C D D E E E F H H I I L N N O O O O S S S T T U U U W
 W U U U T T S S S O O O O N N L I I H H F E E E D D C C C B A A
   A B C D E F H I L N O S T U W
-*///:~
+ *///:~
